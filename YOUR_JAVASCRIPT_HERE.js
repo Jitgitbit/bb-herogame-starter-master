90719ae1e53cf86db52020d6adf88e508fce4816
Jitgitbit <rowowolf@gmail.com>
2020-01-11 17:12:24 +0100

    Cannot read property 'health' of undefined??? why?

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -12,6 +12,7 @@ const hero = {
 };
 
 const person = hero;
+let sleep = document.getElementById('inn');
 
 function rest(person){
   if(person.health !== 10){
@@ -22,10 +23,8 @@ function rest(person){
   };
   return person;
 };
-let sleep = document.getElementById('rest');
+
 sleep.onclick = rest();
-
-
 
 const weapon = {
   type: "sword",
@@ -65,39 +64,61 @@ console.log(weapon);
 console.log('testje');
 const rollerDeck = function(){
   const hero = {
-    name: "Dark Empath",
-    heroic: true,
-    inventory: [],
-    health: 10,
-    weapon: {
-      type: "dagger",
-      damage: 2,
-    },
+  name: "Dark Empath",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "dagger",
+    damage: 2,
+  },
+};
+
+const person = hero;
+let sleep = document.getElementById('inn');
+
+function rest(person){
+  if(person.health !== 10){
+    person.health = 10;
+  }else{
+    alert('Person health is already 10 !');
+    console.log('Person health is already 10 !');
   };
-  
-  const person = hero;
-  
-  function rest(person){
-    if(person.health !== 10){
-      person.health = 10;
-    }else{
-      alert('Person health is already 10 !');
-      console.log('Person health is already 10 !');
-    };
-    return person;
+  return person;
+};
+
+sleep.onclick = rest();
+
+const weapon = {
+  type: "sword",
+  damage: 5,
+};
+console.log('before pick up');
+console.log(person.inventory);
+console.log(person.weapon);
+console.log(weapon);
+
+function pickUpItem(person, weapon){
+  person.inventory.push(weapon);
+  return person.inventory;
+};
+//pickUpItem(person,weapon);
+console.log('after pick up');
+console.log(person.inventory);
+console.log(person.weapon);
+console.log(weapon);
+
+function equipWeapon(person){
+  if(person.inventory !== []){
+    person.weapon = person.inventory[0];
   };
-  
-  const dagger = person.weapon
-  console.log(dagger);
-  
-  function pickUpItem(){
-    let startInventory = person.inventory;
-    console.log(startInventory);
-    person.inventory.push(dagger);
-    return person.inventory;
-  };
-  
-  function equipWeapon(){};
+  return person.weapon;
+};
+//equipWeapon(person);
+console.log('after equip');
+console.log(person.inventory);
+console.log(person.weapon);
+console.log(weapon);
 };
 
 if (
