b4db1a6edf2f0197a6755069a130783439ebcfee
Jitgitbit <rowowolf@gmail.com>
2020-01-11 19:15:57 +0100

    still stuck on the UI

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,5 +1,5 @@
 // Write your JS here
-
+console.log('contentTestje1');
 const hero = {
   name: "Dark Empath",
   heroic: true,
@@ -12,7 +12,6 @@ const hero = {
 };
 
 const person = hero;
-let sleep = document.getElementById('inn');
 
 function rest(person){
   if(person.health !== 10){
@@ -24,41 +23,42 @@ function rest(person){
   return person;
 };
 
-sleep.onclick = rest();
-
 const weapon = {
   type: "sword",
   damage: 5,
 };
-console.log('before pick up');
-console.log(person.inventory);
-console.log(person.weapon);
-console.log(weapon);
 
 function pickUpItem(person, weapon){
   person.inventory.push(weapon);
   return person.inventory;
 };
-//pickUpItem(person,weapon);
-console.log('after pick up');
-console.log(person.inventory);
-console.log(person.weapon);
-console.log(weapon);
+//console.log(pickUpItem(person,weapon));
 
 function equipWeapon(person){
   if(person.inventory !== []){
     person.weapon = person.inventory[0];
   };
-  return person.weapon;
 };
-//equipWeapon(person);
-console.log('after equip');
-console.log(person.inventory);
-console.log(person.weapon);
-console.log(weapon);
+//console.log(equipWeapon(person));
+
+
+let sleep = document.getElementById('inn');
+sleep.onclick = rest;
+/*if(sleep.clicked == true){
+  rest(person);
+}*/
+//sleep.addEventListener('click', rest);
+
+let pickIt = document.getElementById('bag');
+pickIt.onclick = pickUpItem;
+
+let equipIt = document.getElementById('dagger');
+equipIt.onclick = equipWeapon;
 
 
 
+console.log('contentTestjeEinde');
+//eventTarget.addEventListener('click', eventHandlerFunction);
 
 /*
 console.log('testje');
